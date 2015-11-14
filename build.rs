654279f4c79300511701e2efdd45ba99d8ec1059
Wayne Nilsen <waynenilsen@gmail.com>
2015-11-14 12:55:57 -0500

    added friendlier build messages closes #54

--- build.rs
+++ build.rs
@@ -2,14 +2,21 @@ extern crate pkg_config;
 
 use std::env;
 
+fn show_libfuse_msg(lib : &str) {
+	match pkg_config::find_library(lib) {
+		Err(_) => panic!("libfuse is not installed. For OSX use `osxfuse`, for linux use `libfuse-dev` package."),
+		Ok(_) => {}, 
+	}
+}
+
 fn main () {
     let target = env::var("TARGET").unwrap();
     if target.ends_with("-apple-darwin") {
         // Use libosxfuse on OS X
-        pkg_config::find_library("osxfuse").unwrap();
+        show_libfuse_msg("osxfuse"); 
     } else if target.ends_with("-unknown-linux-gnu") || target.ends_with("-unknown-freebsd") {
         // Use libfuse on Linux and FreeBSD
-        pkg_config::find_library("fuse").unwrap();
+        show_libfuse_msg("fuse"); 
     } else {
         // Fail on unsupported platforms (e.g. Windows)
         panic!("Unsupported target platform");
