ff638d1ad9eef10081ac88e4673eaefc4bef1b4f
Jörg Thalheim <joerg@thalheim.io>
2020-10-27 15:36:35 +0100

    make libfuse optional again

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -1,10 +1,13 @@
+#[cfg(feature = "libfuse")]
 #[cfg(not(target_os = "macos"))]
 const LIBFUSE_NAME: &str = "fuse";
 
+#[cfg(feature = "libfuse")]
 #[cfg(target_os = "macos")]
 const LIBFUSE_NAME: &str = "osxfuse";
 
 fn main() {
+    #[cfg(feature = "libfuse")]
     pkg_config::Config::new()
         .atleast_version("2.6.0")
         .probe(LIBFUSE_NAME)
