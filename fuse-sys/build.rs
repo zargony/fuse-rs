cf626a90296a8e447242688b51fa304c6dc03718
Supreet Sethi <supreet.sethi@gmail.com>
2022-08-08 17:38:47 +0800

    no osxfuse specific name changes in macfuse. The library is compatible to freebsd fuse and linux libfuse

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -1,9 +1,5 @@
-/*#[cfg(not(target_os = "macos"))]*/
 const LIBFUSE_NAME: &str = "fuse";
 
-/*#[cfg(target_os = "macos")]
-const LIBFUSE_NAME: &str = "osxfuse";
-*/
 fn main() {
     pkg_config::Config::new()
         .atleast_version("2.6.0")
